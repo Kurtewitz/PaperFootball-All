690d11944c6e155883cdea356c849e5f24a2a8d3
intx <intx@intxs.local>
2018-09-26 21:30:22 +0200

    ready to Merge

--- Server/Server/Connection_Manager.cpp
+++ Server/Server/Connection_Manager.cpp
@@ -59,7 +59,8 @@ void Connection_Manager::connectToGame(Connection::ptr_Connection connection) {
     }
 }
 
-void Connection_Manager::handle_Request(Connection::ptr_Connection connection, std::string request){
+void Connection_Manager::handle_Request(Connection::ptr_Connection connection, std::string request)
+{
     std::stringstream ss(request);
     std::string method, para1, para2, para3 = "";
     std::string instr[20];
@@ -73,26 +74,27 @@ void Connection_Manager::handle_Request(Connection::ptr_Connection connection, s
             ss >> instr[0];
             ss >> instr[0];
             if(instr[0] == "name"){
+                std::cout << "name " << instr[0] << std::endl;
                 instr[2] = instr[0];
                 ss >> instr[0];
                 if (instr[0] == "password"){
+                    std::cout << "pw " << instr[0] << std::endl;
                     instr[3] = instr[0];
                     Account acc{instr[2], instr[3], true};
+                    std::cout << "LoggedIn: " << instr[2] << ", " << instr[3] << std::endl;
                     connection->connectToAccount(acc);
                 }
             }
             this->connectToGame(connection);
+            return;
         }
         else if (method == "TURN"){
-            
         }
         else if (method == "GAMEDATA"){
-            
         }
         else {
             std::cout << "COULD NOT PARSE" << std::endl;
             return;
         }
     }
-    
 }
